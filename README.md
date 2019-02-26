31e67d8b3dc04055c38ff2f5fbf77b3568756900
Anton <web-gambit@mail.ru>
2019-02-26 15:18:28 +0500

    Implement a parity check game

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/08c0e47951269e7e927b/maintainability)](https://codeclimate.com/github/antlu/project-lvl1-s450/maintainability)
 [![Build Status](https://travis-ci.com/antlu/project-lvl1-s450.svg?branch=master)](https://travis-ci.com/antlu/project-lvl1-s450)
+
+[![asciicast](https://asciinema.org/a/2qlboK5OKbSgFxrtRMZSPg4h5.svg)](https://asciinema.org/a/2qlboK5OKbSgFxrtRMZSPg4h5)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
+console.log('Welcome to Brain Games!');
+
+const getName = () => readlineSync.question('May I have your name? ');
+
+// Greeting
+export const sayHello = () => {
+  const name = getName();
   console.log(`Hello, ${name}!`);
+};
+
+// Parity game
+export const evenNumber = () => {
+  const getRandomNumber = () => Math.floor(Math.random() * 101);
+  const isEven = n => (n % 2 === 0 ? 'yes' : 'no');
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".\n');
+
+  const name = getName();
+  const answersToWin = 3;
+  let correctAnswers = 0;
+  let number = getRandomNumber();
+  let answer;
+  let correctAnswer;
+
+  while (correctAnswers < answersToWin) {
+    console.log(`\nQuestion: ${number}`);
+    answer = readlineSync.question('Your answer: ');
+    correctAnswer = isEven(number);
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswers += 1;
+      number = getRandomNumber();
+    } else {
+      console.log(`${answer} is a wrong answer. The correct answer is ${correctAnswer}.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
 };
