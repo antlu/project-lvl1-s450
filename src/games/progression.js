bcef2ac6a3e6d7e246604d9470148c1705f0fd94
Anton <web-gambit@mail.ru>
2019-02-27 19:10:18 +0500

    Fix names for the progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,7 +1,7 @@
 import { getRandomNumber } from '../utils';
 import launchGame from '..';
 
-const getStringOfNumbers = (start, step = 1, n = 10) => {
+const getSequenceOfNumbers = (start, step = 1, n = 10) => {
   const sequence = [];
   for (let i = 0, j = start; i < n; i += 1, j += step) {
     sequence[i] = j;
@@ -11,11 +11,11 @@ const getStringOfNumbers = (start, step = 1, n = 10) => {
 
 const goal = 'What number is missing in the progression?\n';
 const getQuestionAndAnswer = () => {
-  const progression = getStringOfNumbers(getRandomNumber(0, 20), getRandomNumber(-5, 5));
-  const i = getRandomNumber(0, progression.length - 1);
-  const answer = progression[i];
-  progression[i] = '..';
-  const question = progression.join(' ');
+  const sequence = getSequenceOfNumbers(getRandomNumber(0, 20), getRandomNumber(-5, 5));
+  const i = getRandomNumber(0, sequence.length - 1);
+  const answer = sequence[i];
+  sequence[i] = '..';
+  const question = sequence.join(' ');
   return [question, String(answer)];
 };
 
